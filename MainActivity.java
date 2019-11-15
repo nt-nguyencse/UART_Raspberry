d4b5ab5c34f6ee6a9e423112c6f1d426ae6eddda
ntnguyencse2017 <conloctheki1998@gmail.com>
2019-11-15 11:17:26 +0700

    Main Activity

---
+++ MainActivity.java
@@ -0,0 +1,191 @@
+package com.ntn1612293.uart;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.android.things.pio.PeripheralManager;
+import com.google.android.things.pio.UartDevice;
+import com.google.android.things.pio.UartDeviceCallback;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+import static java.lang.String.valueOf;
+
+/**
+ * Skeleton of an Android Things activity.
+ * <p>
+ * Android Things peripheral APIs are accessible through the class
+ * PeripheralManagerService. For example, the snippet below will open a GPIO pin and
+ * set it to HIGH:
+ *
+ * <pre>{@code
+ * PeripheralManagerService service = new PeripheralManagerService();
+ * mLedGpio = service.openGpio("BCM6");
+ * mLedGpio.setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW);
+ * mLedGpio.setValue(true);
+ * }</pre>
+ * <p>
+ * For more complex peripherals, look for an existing user-space driver, or implement one if none
+ * is available.
+ *
+ * @see <a href="https://github.com/androidthings/contrib-drivers#readme">https://github.com/androidthings/contrib-drivers#readme</a>
+ */
+
+public class MainActivity extends Activity {
+    private static final String TAG = "LoopbackActivity";
+    TextView text;
+    // UART Configuration Parameters
+    private static final int BAUD_RATE = 115200;
+    private static final int DATA_BITS = 8;
+    private static final int STOP_BITS = 1;
+
+    private static final int CHUNK_SIZE = 512;
+
+    private HandlerThread mInputThread;
+    private Handler mInputHandler;
+
+    private UartDevice mLoopbackDevice;
+
+    private Runnable mTransferUartRunnable = new Runnable() {
+        @Override
+        public void run() {
+            //transferUartData();
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=(TextView)findViewById(R.id.response);
+        Log.d(TAG, "Loopback Created");
+
+        // Create a background looper thread for I/O
+        mInputThread = new HandlerThread("InputThread");
+        mInputThread.start();
+        mInputHandler = new Handler(mInputThread.getLooper());
+
+        // Attempt to access the UART device
+        try {
+            openUart("UART0", BAUD_RATE);
+            // Read any initially buffered data
+            //mInputHandler.post(mTransferUartRunnable);
+            Log.d(TAG, "UART Open ok");
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to open UART device", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Loopback Destroyed");
+
+        // Terminate the worker thread
+        if (mInputThread != null) {
+            mInputThread.quitSafely();
+        }
+
+        // Attempt to close the UART device
+        try {
+            closeUart();
+        } catch (IOException e) {
+            Log.e(TAG, "Error closing UART device:", e);
+        }
+    }
+
+    /**
+     * Callback invoked when UART receives new incoming data.
+     */
+    private UartDeviceCallback mCallback = new UartDeviceCallback() {
+        @Override
+        public boolean onUartDeviceDataAvailable(UartDevice uart) {
+            // Queue up a data transfer
+            transferUartData();
+            //Continue listening for more interrupts
+            Log.d(TAG, "abc");
+            return true;
+        }
+
+        @Override
+        public void onUartDeviceError(UartDevice uart, int error) {
+            Log.w(TAG, uart + ": Error event " + error);
+        }
+    };
+
+    /* Private Helper Methods */
+
+    /**
+     * Access and configure the requested UART device for 8N1.
+     *
+     * @param name Name of the UART peripheral device to open.
+     * @param baudRate Data transfer rate. Should be a standard UART baud,
+     *                 such as 9600, 19200, 38400, 57600, 115200, etc.
+     *
+     * @throws IOException if an error occurs opening the UART port.
+     */
+    private void openUart(String name, int baudRate) throws IOException {
+        mLoopbackDevice = PeripheralManager.getInstance().openUartDevice("UART0");
+        // Configure the UART
+        mLoopbackDevice.setBaudrate(baudRate);
+        mLoopbackDevice.setDataSize(DATA_BITS);
+        mLoopbackDevice.setParity(UartDevice.PARITY_NONE);
+        mLoopbackDevice.setStopBits(STOP_BITS);
+
+        mLoopbackDevice.registerUartDeviceCallback(mCallback);
+    }
+
+    /**
+     * Close the UART device connection, if it exists
+     */
+    private void closeUart() throws IOException {
+        if (mLoopbackDevice != null) {
+            mLoopbackDevice.unregisterUartDeviceCallback(mCallback);
+            try {
+                mLoopbackDevice.close();
+            } finally {
+                mLoopbackDevice = null;
+            }
+        }
+    }
+
+    /**
+     * Loop over the contents of the UART RX buffer, transferring each
+     * one back to the TX buffer to create a loopback service.
+     *
+     * Potentially long-running operation. Call from a worker thread.
+     */
+    private void transferUartData() {
+        if (mLoopbackDevice != null) {
+            // Loop until there is no more data in the RX buffer.
+            try {
+                byte[] buffer = new byte[512];
+                int read;
+                while ((read = mLoopbackDevice.read(buffer, buffer.length)) > 0) {
+                      Log.d(TAG, "Read " + read + " bytes from peripheral ---"+new String(buffer) );
+
+                       // text.setText("Received "+read+" bytes");
+                    //mLoopbackDevice.write(buffer,read);
+                    Log.d("Received: ","+" + read );
+                }
+
+
+
+
+            } catch (IOException e) {
+                Log.w(TAG, "Unable to transfer data over UART", e);
+            }
+        }
+    }
+};
